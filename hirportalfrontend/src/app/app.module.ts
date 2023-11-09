1f2590d047f9c126d30e2e0b0bfd26e32c81c6c6
Farkas Zoltán Gáspár <zoltan.farkas1028@gmail.com>
2023-11-09 23:10:47 +0100

    fooldal component added

--- hirportalfrontend/src/app/app.module.ts
+++ hirportalfrontend/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooldalComponent } from './fooldal/fooldal.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'fooldal', pathMatch: 'full'},
+  {path: 'fooldal', component: FooldalComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooldalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ hirportalfrontend/src/app/fooldal/fooldal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hp-fooldal',
+  templateUrl: './fooldal.component.html',
+  styleUrls: ['./fooldal.component.scss']
+})
+export class FooldalComponent {
+
+}
